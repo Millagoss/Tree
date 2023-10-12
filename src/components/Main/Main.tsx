1f5de07e011ab7075fad0e06317f2d0658c09d0d
Million Gossaye <millagoss19@gmail.com>
2023-10-12 15:30:25 +0300

    main section, with arrows added.

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,70 @@
+import { useState } from "react";
+
+import {
+  AngleUp,
+  AngleDown,
+  AngleLeft,
+  AngleRight,
+} from "../../utils/icons/Icon";
+
+import "./main.css";
+
 const Main = () => {
-  return <div id="main">Main</div>;
+  const [position, setPosition] = useState({ x: 700, y: 150 });
+  const [dragging, setDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (e: any) => {
+    setDragging(true);
+    setOffset({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    });
+  };
+
+  const handleMouseMove = (e: any) => {
+    if (!dragging) return;
+    setPosition({
+      x: e.clientX - offset.x,
+      y: e.clientY - offset.y,
+    });
+  };
+
+  const handleMouseUp = () => {
+    setDragging(false);
+  };
+
+  return (
+    <main>
+      <div className="container" id="main">
+        <div
+          className="box"
+          style={{
+            left: position.x,
+            top: position.y,
+            cursor: dragging ? "grabbing" : "grab",
+          }}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+        >
+          Drag me!
+        </div>
+      </div>
+      <div className="up-arrow arrow">
+        <AngleUp />
+      </div>
+      <div className="right-arrow arrow">
+        <AngleRight />
+      </div>
+      <div className="down-arrow arrow">
+        <AngleDown />
+      </div>
+      <div className="left-arrow arrow">
+        <AngleLeft />
+      </div>
+    </main>
+  );
 };
 
 export default Main;
--- src/utils/icons/Icon.js
+++ src/utils/icons/Icon.js
@@ -1,5 +1,17 @@
 import { ReactComponent as MinusIcon } from "./icon-minus.svg";
 import { ReactComponent as PlusIcon } from "./icon-plus.svg";
 import { ReactComponent as CursorIcon } from "./icon-cursor.svg";
+import { ReactComponent as AngleUp } from "./angle-up.svg";
+import { ReactComponent as AngleDown } from "./angle-down.svg";
+import { ReactComponent as AngleLeft } from "./angle-left.svg";
+import { ReactComponent as AngleRight } from "./angle-right.svg";
 
-export { MinusIcon, PlusIcon, CursorIcon };
+export {
+  MinusIcon,
+  PlusIcon,
+  CursorIcon,
+  AngleUp,
+  AngleDown,
+  AngleLeft,
+  AngleRight,
+};
